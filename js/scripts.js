5e3b62bcd35a47f8c494ebad70439f5485060dab
Daniela Trulls and James Lannon <james@>
2017-05-04 12:00:58 -0700

    make an array from the userInput value

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var romanNumerals = ["I", "V", "X", "L", "C", "D", "M"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
+var numberArray = [];
 
 var one = "I";
 var five = "V";
@@ -11,21 +12,52 @@ var fiveHundred = "D";
 var thousand = "M";
 
 var singleCharacters = function(number){
-  if (number === 0){
+  if (number == 0){
   return " ";
-} else if (number === 1){
+} else if (number[-1] == 1){
   return one;
-} else if (number === 2){
+} else if (number == 2){
   return one + one;
-} else if (number === 3){
+} else if (number == 3){
   return one + one + one;
-} else if (number === 4){
+} else if (number == 4){
   return one + five ;
-} else if (number === 5){
+} else if (number == 5){
   return five ;
+} else if (number == 6){
+  return five + one;
+} else if (number == 7){
+  return five + one + one;
+} else if (number == 8){
+  return five + one + one + one;
+} else if (number == 9){
+  return one + ten ;
+}
+}
 
+var makeArray = function(number1){
+var splitUserInput = number1.split("");
+numberArray.push(splitUserInput);
+console.log(numberArray);
 }
-}
+// now on to tens
+
+// var secondCharacters = function(number){
+//   if (number === 0){
+//   return " ";
+// } else if (number === 1){
+//   return ten;
+// } else if (number === 2){
+//   return ten + ten;
+// } else if (number === 3){
+//   return ten + ten + ten;
+// } else if (number === 4){
+//   return ten + fifty ;
+// } else if (number === 5){
+//   return fifty ;
+// }
+//
+// }
 // for(var index = 0; index < userInput.length; index += 1){
 //   if (userInput[-1] === numbers[index]){
 //   }  ;
@@ -43,10 +75,14 @@ $(document).ready(function(){
   $("#roman-numeral").submit(function(event){
     event.preventDefault();
 
-    var userInput = parseInt($("#userNumber").val());
+    var userInput = $("#userNumber").val();
     var output = singleCharacters(userInput);
 
+    makeArray(userInput);
     console.log(userInput);
+
+
+    // console.log(userInput);
     $("#result").text(output);
   });
 });
