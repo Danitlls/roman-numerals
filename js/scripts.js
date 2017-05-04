25abccc6a79bc165a89e171c6f23032ca913dad1
Daniela Trulls and James Lannon <daniela@>
2017-05-04 16:26:24 -0700

    try differents things that did not work

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,17 @@
-var romanNumerals = ["I", "V", "X", "L", "C", "D", "M"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+// var romanNumerals = ["I", "V", "X", "L", "C", "D", "M"];
+// var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 
-var numberArray = [];
-
-// var M = numberArray[-4];
-// var C = numberArray[-3];
-// var D = numberArray[-2];
-// var U = numberArray[-1];
-//
-//
-// var romanNumeralDisplay = function(number){
-//   if (U = true) {
-//   return true
-// }
-// }
 
 
-// var one = "I";
-// var five = "V";
-// var ten = "X";
-// var fifty = "L";
-// var hundred = "C";
-// var fiveHundred = "D";
-// var thousand = "M";
-//
+var one = "I";
+var five = "V";
+var ten = "X";
+var fifty = "L";
+var hundred = "C";
+var fiveHundred = "D";
+var thousand = "M";
+
 // var singleCharacters = function(number){
 //   if (number == 0){
 //   return " ";
@@ -48,83 +35,48 @@ var numberArray = [];
 //     return one + ten ;
 //   }
 // }
+var splitUserInput = [];
 
-var makeArray = function(number1){
-var splitUserInput = number1.split("");
-numberArray.push(splitUserInput);
-console.log(numberArray);
-}
-
-// now on to tens
-
-// var secondCharacters = function(number){
-//   if (number === 0){
-//   return " ";
-// } else if (number === 1){
-//   return ten;
-// } else if (number === 2){
-//   return ten + ten;
-// } else if (number === 3){
-//   return ten + ten + ten;
-// } else if (number === 4){
-//   return ten + fifty ;
-// } else if (number === 5){
-//   return fifty ;
+// var makeArray = function(number1){
+// splitUserInput = number1.split("");
+// numberArray11111111.push(splitUserInput);
+// console.log(numberArray);
 // }
-//
-// }
-var numerals = ["I", "II", "III", "IV", "V", "VI","VII", "VIII", "IX"];
 
-var numbers = [1, 2, 3, 4, 5, 6,7, 8, 9];
+var numerals = ["", "I", "II", "III", "IV", "V", "VI","VII", "VIII", "IX"];
+var numbers = [0, 1, 2, 3, 4, 5, 6,7, 8, 9];
+var numberArray = [];
 
 var someFunc = function(input) {
-  var newArray = [];
+  // makeArray();
+for(var index = 0; index < numbers.length; index += 1){
+  // if (numbers[index] === input){
+  //   numberArray.push(numerals[numbers[index]]);
+  // }
 
-  for(var index = 0; index < numbers.length; index += 1){
-    if (numbers[index] === input){
-      newArray.push(numberals[numbers[index]])
-    }
-    // if (numberArray[numberArray.length - 2] === numbers[index]){
-    //
-    // }
-  };
-  // return newArray.join()
-  return newArray;
+  if (numbers[index] === input){
+    numberArray.push(numerals[numbers[index]]);
+    console.log(numberArray);
+  }
+
+  // else{
+  //
+  // }
+};
+// return newArray.join()
+  return numberArray;
+
 }
 
-// for(var index = 0; index < numberArray.length; index += 1){
-//   if (numberArray[numberArray.length - 1] === numbers[index]){
-//     foo.push(nu)
-//
-//   }
-//
-//   if (numberArray[numberArray.length - 2] === numbers[index]){
-//
-//
-//   }
-//   };
-
-
-
-// for(var index = 0; index < userInput.length; index += 1){
-//   if (singleCharacters(number) === romanNumerals[index]){
-//     ;
-//   }
-//   console.log(singleCharacters);
-// }
+// -------------------------------------------------
 
 $(document).ready(function(){
   $("#roman-numeral").submit(function(event){
     event.preventDefault();
 
-    var userInput = $("#userNumber").val();
+    var userInput = parseInt($("#userNumber").val());
     var output = someFunc(userInput);
 
-    makeArray(userInput);
-    // console.log(makeArray(userInput));
-
-
-    someFunc(userInput);
 
 
     $("#result").text(output);
